cbd8901cac451f4340e7fa6e1a19f500b725569d
92labs320 <92labs320@gmail.com>
2026-05-24 23:28:23 -0400

    Finalize App Store safe settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -115,7 +115,6 @@ export default function Settings() {
       </Card>
 
       <Card title="Preferences">
-        <Item label="Notifications" value="Off" />
         <UnitPreference
           unitSystem={unitSystem}
           onChange={setUnitSystem}
